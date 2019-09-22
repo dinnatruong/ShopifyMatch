a801dab5a26ce3c7a334a8ef2d647a39e674265d
dinnatruong <dinnatruong@gmail.com>
2019-09-22 14:49:21 -0400

    Created help and winner AlertDialogs

--- app/src/main/java/com/example/shopifymatch/view/ui/GameActivity.java
+++ app/src/main/java/com/example/shopifymatch/view/ui/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.shopifymatch.view.ui;
 
+import android.app.AlertDialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
@@ -9,10 +10,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.shopifymatch.R;
@@ -76,6 +79,10 @@ public class GameActivity extends AppCompatActivity implements MatchingCardAdapt
             public void onChanged(@Nullable Integer integer) {
                 TextView numPairsLeft = findViewById(R.id.numPairsLeft);
                 numPairsLeft.setText(String.valueOf(integer));
+
+                if (integer != null && integer == 0) {
+                    showWinnerDialog();
+                }
             }
         });
     }
@@ -123,8 +130,54 @@ public class GameActivity extends AppCompatActivity implements MatchingCardAdapt
                 break;
 
             case R.id.action_help:
+                showHelpDialog();
                 break;
         }
         return true;
+    }
+
+    private void showWinnerDialog() {
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_winner, viewGroup, false);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogView);
+
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.setCancelable(false);
+        alertDialog.setCanceledOnTouchOutside(false);
+
+        dialogView.findViewById(R.id.playAgainBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cardMatchingViewModel.restartCards();
+                alertDialog.dismiss();
+            }
+        });
+
+        dialogView.findViewById(R.id.homeBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        alertDialog.show();
+    }
+
+    private void showHelpDialog() {
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_help, viewGroup, false);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogView);
+
+        final AlertDialog alertDialog = builder.create();
+        dialogView.findViewById(R.id.closeBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.dismiss();
+            }
+        });
+
+        alertDialog.show();
     }
 }
--- app/src/main/java/com/example/shopifymatch/view/ui/MainActivity.java
+++ app/src/main/java/com/example/shopifymatch/view/ui/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.shopifymatch.view.ui;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.shopifymatch.R;
@@ -21,6 +24,27 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        Button helpBtn = findViewById(R.id.helpBtn);
+        helpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ViewGroup viewGroup = findViewById(android.R.id.content);
+                View dialogView = LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog_help, viewGroup, false);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setView(dialogView);
+
+                final AlertDialog alertDialog = builder.create();
+                dialogView.findViewById(R.id.closeBtn).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        alertDialog.dismiss();
+                    }
+                });
+
+                alertDialog.show();
             }
         });
     }
